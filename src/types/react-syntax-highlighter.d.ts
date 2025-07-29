5092ff16f8819247db985fc791472419b0d4c933
kaito <kaito>
2025-07-29 13:48:33 +0900

    commit11

--- src/types/react-syntax-highlighter.d.ts
+++ src/types/react-syntax-highlighter.d.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "react-syntax-highlighter" {
   export const Prism: {
     [key: string]: any;
   };
 }
+/* eslint-enable @typescript-eslint/no-explicit-any */
